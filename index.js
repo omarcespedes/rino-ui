eda1b3f2907eb05f52f648e0e4084dc063e3ee6b
Omar <omarcspds@gmail.com>
2021-05-04 17:34:13 -0400

    initial version of webpack

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 import TextField from './src/components/TextField/TextField';
+import Button from './src/components/Button/Button';
+import ContentContainer from './src/components/ContentContainer/ContentContainer';
+import DatePicker from './src/components/DatePicker/DatePicker';
+import Icon from './src/components/Icon/Icon';
+import NavBar from './src/components/NavBar/NavBar';
+import Select from './src/components/Select/Select';
+import SelectOption from './src/components/SelectOption/SelectOption';
+import SideMenu from './src/components/SideMenu/SideMenu';
+import Table from './src/components/Table/Table';
 
-export { TextField };
+export {
+    TextField,
+    Button,
+    ContentContainer,
+    DatePicker,
+    Icon,
+    NavBar,
+    Select,
+    SelectOption,
+    SideMenu,
+    Table,
+};
